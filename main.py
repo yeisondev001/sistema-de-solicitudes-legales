8be4be4f0e2677e72c2aa1410f76304c7d5bc447
Yeison Gregori Rojas Henriquez <yeisonrojas03@gmail.com>
2026-05-25 23:22:15 -0400

    Use PORT env variable for Render.com deployment

--- main.py
+++ main.py
@@ -1474,4 +1474,6 @@ def main(page: ft.Page):
 
 
 if __name__ == "__main__":
-    ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=port)
